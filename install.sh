9e834596861c2a35469a844e43cafbdaefd62ea1
kmirzavaziri <kmirzavaziri@gmail.com>
2021-09-23 10:19:17 +0330

    now installable

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ $EUID -ne 0 ]; then
+    echo -e "You need to have root privileges to run this script.\nPlease run 'sudo ./install.sh'."
+    exit
+fi
+
+XUA_CLI_DIR=/usr/bin/xua-cli
+CLI_DIR=/usr/bin
+rm -rf $XUA_CLI_DIR
+cp -r xua $XUA_CLI_DIR
+ln -sf $XUA_CLI_DIR/xua $CLI_DIR/xua
