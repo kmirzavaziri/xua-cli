0566a099bc8825db7c9796c0aa01d51cb1a73387
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2021-10-19 02:02:44 +0330

    fix

--- xua/builders/doc/htmlOld.py
+++ xua/builders/doc/htmlOld.py
@@ -5,61 +5,8 @@ from weasyprint import HTML
 from xua.builders.eve import BuildEngineEve
 from xua.exceptions import UserError
 from xua.constants import CONFIG, BUILD
-from xua.builders.doc.lex import tokenize
 
 SINGLE_QUOTE = "'"
-
-
-class engine(BuildEngineEve):
-    def __init__(self, config):
-        super().__init__(config)
-        self.toc = {}
-        linear = TocNode.getNodes(config)
-        for node in linear:
-            self.toc[node.path] = node
-
-    def _project(self):
-        return CONFIG.KEY.PROJECT_DOC_HTML
-
-    def _build(self, path):
-        # level = self.toc[path].level if path in self.toc else path.count(
-        #     os.path.sep)
-
-        with open(path, 'r') as f:
-            source = f.read()
-        source = self.getComments(source)
-        result = tokenize(source)
-        return '\n'.join(map(str, result))
-
-    def getComments(self, source):
-        comments = []
-        sourceLines = source.splitlines()
-        for i in range(len(sourceLines)):
-            hashIndex = sourceLines[i].find('#')
-            comment = sourceLines[i][hashIndex + 1:] if hashIndex >= 0 else ''
-            if i > 0 and sourceLines[i - 1] and sourceLines[i - 1][-1] == '\\':
-                comments[-1] += '\n' + comment
-            else:
-                comments.append(comment)
-        return '\n'.join(comments)
-
-    def _buildBase(self):
-        self.__buildPdf()
-
-    def __buildPdf(self):
-        if CONFIG.KEY.PDF_PATH not in self.config.config[CONFIG.KEY.PROJECTS][CONFIG.KEY.PROJECT_DOC_HTML]:
-            return
-
-        pdfPath = os.path.join(self.config.config[CONFIG.KEY.PROJECTS][CONFIG.KEY.PROJECT_DOC_HTML][CONFIG.KEY.BUILD_DIR],
-                               self.config.config[CONFIG.KEY.PROJECTS][CONFIG.KEY.PROJECT_DOC_HTML][CONFIG.KEY.PDF_PATH])
-
-        book = ''
-        for node in self.toc:
-            with open(self.config.getCorrespondingPath(CONFIG.KEY.PROJECT_DOC_HTML, node, BUILD.MAP_PROJECT_EXTENSION[CONFIG.KEY.PROJECT_DOC_HTML])) as f:
-                book += f.read()
-
-        HTML(string=book, base_url=self.config.config[CONFIG.KEY.PROJECTS]
-             [CONFIG.KEY.PROJECT_DOC_HTML][CONFIG.KEY.BUILD_DIR]).write_pdf(pdfPath)
 
 
 class BuildEngine(BuildEngineEve):
